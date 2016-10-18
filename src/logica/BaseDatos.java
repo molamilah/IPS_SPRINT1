8bef94aa1a31d3b2a27fb1dbd786ad9c663c3716
DD <DD@DD-MPC>
2016-10-18 18:07:55 +0200

    Rollos de david

--- src/logica/BaseDatos.java
+++ src/logica/BaseDatos.java
@@ -1,5 +1,9 @@
 package logica;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -26,6 +30,22 @@ public class BaseDatos {
 	@SuppressWarnings("serial")
 	public class ExcepcionUsuarioNoEncontrado extends Exception {
 		public ExcepcionUsuarioNoEncontrado(String msg) {
+			super(msg);
+		}
+
+	}
+	
+	@SuppressWarnings("serial")
+	public class ExcepcionReservaNoEncontrada extends Exception {
+		public ExcepcionReservaNoEncontrada(String msg) {
+			super(msg);
+		}
+
+	}
+	
+	@SuppressWarnings("serial")
+	public class ExcepcionPagoNoEncontrado extends Exception {
+		public ExcepcionPagoNoEncontrado(String msg) {
 			super(msg);
 		}
 
@@ -252,4 +272,167 @@ public class BaseDatos {
 		}
 		return false;
 	}
+	
+	private int findID_Usuario(String DNI) throws ExcepcionUsuarioNoEncontrado {
+		Connection con = null;
+		PreparedStatement psID_USUARIO = null;
+		ResultSet rsID_USUARIO = null;
+		int idUsuario = 0;
+		try {
+			con = conectar();
+			psID_USUARIO = con.prepareStatement("select id_usuario from usuario where DNI = ?");
+			psID_USUARIO.setString(1, DNI);
+			rsID_USUARIO = psID_USUARIO.executeQuery();
+			if(rsID_USUARIO.next()) {
+				idUsuario = rsID_USUARIO.getInt(1);
+				rsID_USUARIO.close();
+				psID_USUARIO.close();
+				con.close();
+			} else {
+				rsID_USUARIO.close();
+				psID_USUARIO.close();
+				con.close();
+				throw new ExcepcionUsuarioNoEncontrado("El usuario no se encuentra en la base de datos");
+			}
+			return idUsuario;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return idUsuario;
+	}
+	
+	private int findID_Reserva(int id_usuario, Timestamp fecha) throws ExcepcionReservaNoEncontrada {
+		Connection con = null;
+		PreparedStatement psID_RESERVA = null;
+		ResultSet rsID_RESERVA = null;
+		
+		int idReserva = 0;
+		
+		try {
+			con = conectar();
+			psID_RESERVA = con.prepareStatement("select id_reserva from reserva "
+					+ "where id_usuario = ? and hora_inicio = ?");
+			psID_RESERVA.setInt(1, id_usuario);
+			psID_RESERVA.setTimestamp(2, fecha);
+			rsID_RESERVA = psID_RESERVA.executeQuery();
+			if(rsID_RESERVA.next()) {
+				idReserva = rsID_RESERVA.getInt(1);
+				rsID_RESERVA.close();
+				psID_RESERVA.close();
+				con.close();
+			} else {
+				rsID_RESERVA.close();
+				psID_RESERVA.close();
+				con.close();
+				throw new ExcepcionReservaNoEncontrada("La reserva no se encuentra en la base de datos");
+			}
+			return idReserva;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return idReserva;
+	}
+	
+	private int findID_Pago( int idReserva) throws ExcepcionPagoNoEncontrado {
+		Connection con = null;
+		PreparedStatement psID_PAGO = null;
+		ResultSet rsID_PAGO = null;
+		
+		int idPago = 0;
+		
+		try {
+			con = conectar();
+			psID_PAGO = con.prepareStatement("select id_pago from pago where id_reserva = ?");
+			psID_PAGO.setInt(1, idReserva);
+			rsID_PAGO = psID_PAGO.executeQuery();
+			if(rsID_PAGO.next()) {
+				idPago = rsID_PAGO.getInt(1);
+				rsID_PAGO.close();
+				psID_PAGO.close();
+				con.close();
+			} else {
+				rsID_PAGO.close();
+				psID_PAGO.close();
+				con.close();
+				throw new ExcepcionPagoNoEncontrado("El pago no se encuentra en la base de datos");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return idPago;
+	}
+	
+	public void pasarPagoAPagado( String DNI, Timestamp fecha ) throws ExcepcionUsuarioNoEncontrado, ExcepcionReservaNoEncontrada, ExcepcionPagoNoEncontrado, IOException {
+		Connection con = null;		
+		PreparedStatement psUPDATE_PAGO = null;
+		
+		int id_usuario = 0;
+		int id_reserva = 0;
+		int id_pago = 0;
+		
+		try {
+			con = conectar();
+			id_usuario = findID_Usuario(DNI);
+			id_reserva = findID_Reserva(id_usuario, fecha);
+			
+			psUPDATE_PAGO = con.prepareStatement("update pago set status = ? where id_reserva = ?");
+			psUPDATE_PAGO.setString(1, "pagado");
+			psUPDATE_PAGO.setInt(2, id_reserva);
+			int num = psUPDATE_PAGO.executeUpdate();
+			if( num == 0) {
+				throw new ExcepcionPagoNoEncontrado("El pago no existe");
+			}
+			id_pago = findID_Pago(id_reserva);
+			generarRecibo(id_pago, DNI, fecha);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private double sacarImportePago(int idPago) {
+		Connection con = null;
+		PreparedStatement psIMPORTE_PAGO = null;
+		ResultSet rsIMPORTE_PAGO = null;
+		
+		double importe = 0.0;
+		
+		try {
+			con = conectar();
+			psIMPORTE_PAGO = con.prepareStatement("select precio from pago where id_pago = ?");
+			psIMPORTE_PAGO.setInt(1, idPago);
+			rsIMPORTE_PAGO = psIMPORTE_PAGO.executeQuery();
+			rsIMPORTE_PAGO.next();
+			importe = rsIMPORTE_PAGO.getDouble(1);
+			
+			rsIMPORTE_PAGO.close();
+			psIMPORTE_PAGO.close();
+			con.close();
+		} catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		return importe;
+	}
+	
+	private void generarRecibo( int id_pago, String DNI, Timestamp fecha) throws IOException {
+		int idPago = id_pago;
+		double importe = sacarImportePago(idPago);
+		
+		String ruta = id_pago + ".txt";
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(archivo.exists()) {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("Número de recibo: " + idPago);
+            bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
+            bw.write("Importe a pagar: " + importe + ".");
+        } else {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("Número de recibo: " + idPago);
+            bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
+            bw.write("Importe a pagar: " + importe + ".");
+        }
+        bw.close();
+	}
+	
 }
