195d17d57a2a6032289f9a642fde42964d8932a8
DD <DD@100.64.81.140>
2016-11-01 16:26:42 +0100

    Combio en la base de datos, se añade el campo estado a la reserva.

--- src/iguReserva/VentanaReserasPropiasUsuario.java
+++ src/iguReserva/VentanaReserasPropiasUsuario.java
@@ -3,37 +3,79 @@ package iguReserva;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
+import logica.BaseDatos;
 import logica.Usuario;
 
 import javax.swing.JRadioButton;
 import java.awt.Font;
 import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import java.awt.event.ActionListener;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.awt.event.ActionEvent;
 
 public class VentanaReserasPropiasUsuario extends JDialog {
-	
+
 	private static final long serialVersionUID = 6735821194521131468L;
 	private JPanel pnFiltro;
 	private JRadioButton rdbtnCanceladas;
 	private JRadioButton rdbtnPendientes;
 	private JRadioButton rdbtnRealizadas;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	
+
 	private Usuario usuario;
+	private BaseDatos bd;
+	private JPanel pnFechas;
+	private JComboBox<String> cbDia;
+	private JComboBox<String> cbMes;
+	private JComboBox<String> cbAno;
+	private JLabel lblDesde;
+	private JComboBox<String> cbDiaH;
+	private JComboBox<String> cbMesH;
+	private JComboBox<String> cbAnoH;
+	private JLabel lblHasta;
+	private JButton btnMostrarReserva;
+	private JButton btnAnularReserva;
+	private JButton btnAtras;
+	private DefaultTableModel modeloTablaReservas;
+	private JScrollPane scrollPane;
+	private JTable tbReservas;
+
+	private Calendar c = Calendar.getInstance();
+	private int dia = c.get(Calendar.DATE);
+	private int mes = c.get(Calendar.MONTH);
+	private int año = c.get(Calendar.YEAR);
+
 	/**
 	 * Create the dialog.
 	 */
 	public VentanaReserasPropiasUsuario(Usuario usuario) {
 		this.usuario = usuario;
+		bd = new BaseDatos();
 		setTitle("Reservas Propias");
-		setBounds(100, 100, 678, 536);
+		setBounds(100, 100, 683, 602);
 		getContentPane().setLayout(null);
 		getContentPane().add(getPnFiltro());
+		getContentPane().add(getPnFechas());
+		getContentPane().add(getBtnMostrarReserva());
+		getContentPane().add(getBtnAnularReserva());
+		getContentPane().add(getBtnAtras());
+		getContentPane().add(getScrollPane_1());
 	}
+
 	private JPanel getPnFiltro() {
 		if (pnFiltro == null) {
 			pnFiltro = new JPanel();
-			pnFiltro.setBorder(new TitledBorder(null, "Filtro Reservas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			pnFiltro.setBorder(
+					new TitledBorder(null, "Filtro Reservas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			pnFiltro.setBounds(10, 11, 642, 69);
 			pnFiltro.setLayout(null);
 			pnFiltro.add(getRdbtnCanceladas());
@@ -42,6 +84,7 @@ public class VentanaReserasPropiasUsuario extends JDialog {
 		}
 		return pnFiltro;
 	}
+
 	private JRadioButton getRdbtnCanceladas() {
 		if (rdbtnCanceladas == null) {
 			rdbtnCanceladas = new JRadioButton("Canceladas");
@@ -51,6 +94,7 @@ public class VentanaReserasPropiasUsuario extends JDialog {
 		}
 		return rdbtnCanceladas;
 	}
+
 	private JRadioButton getRdbtnPendientes() {
 		if (rdbtnPendientes == null) {
 			rdbtnPendientes = new JRadioButton("Pendientes");
@@ -61,6 +105,7 @@ public class VentanaReserasPropiasUsuario extends JDialog {
 		}
 		return rdbtnPendientes;
 	}
+
 	private JRadioButton getRdbtnRealizadas() {
 		if (rdbtnRealizadas == null) {
 			rdbtnRealizadas = new JRadioButton("Realizadas");
@@ -69,5 +114,186 @@ public class VentanaReserasPropiasUsuario extends JDialog {
 			rdbtnRealizadas.setBounds(44, 27, 109, 23);
 		}
 		return rdbtnRealizadas;
+	}
+
+	private JPanel getPnFechas() {
+		if (pnFechas == null) {
+			pnFechas = new JPanel();
+			pnFechas.setBorder(new TitledBorder(null, "Fechas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			pnFechas.setBounds(10, 104, 642, 91);
+			pnFechas.setLayout(null);
+			pnFechas.add(getCbDia());
+			pnFechas.add(getCbMes());
+			pnFechas.add(getCbAno());
+			pnFechas.add(getLblDesde());
+			pnFechas.add(getCbDiaH());
+			pnFechas.add(getCbMesH());
+			pnFechas.add(getCbAnoH());
+			pnFechas.add(getLblHasta());
+		}
+		return pnFechas;
+	}
+
+	private JComboBox<String> getCbDia() {
+		if (cbDia == null) {
+			cbDia = new JComboBox<String>();
+			cbDia.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			cbDia.setBounds(10, 47, 59, 25);
+		}
+		return cbDia;
+	}
+
+	private JComboBox<String> getCbMes() {
+		if (cbMes == null) {
+			cbMes = new JComboBox<String>();
+			cbMes.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			cbMes.setBounds(79, 47, 129, 25);
+		}
+		return cbMes;
+	}
+
+	private JComboBox<String> getCbAno() {
+		if (cbAno == null) {
+			cbAno = new JComboBox<String>();
+			cbAno.setBounds(218, 47, 84, 25);
+		}
+		return cbAno;
+	}
+
+	private JLabel getLblDesde() {
+		if (lblDesde == null) {
+			lblDesde = new JLabel("Desde:");
+			lblDesde.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblDesde.setBounds(10, 22, 71, 25);
+		}
+		return lblDesde;
+	}
+
+	private JComboBox<String> getCbDiaH() {
+		if (cbDiaH == null) {
+			cbDiaH = new JComboBox<String>();
+			cbDiaH.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			cbDiaH.setBounds(329, 47, 59, 25);
+		}
+		return cbDiaH;
+	}
+
+	private JComboBox<String> getCbMesH() {
+		if (cbMesH == null) {
+			cbMesH = new JComboBox<String>();
+			cbMesH.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			cbMesH.setBounds(398, 47, 129, 25);
+		}
+		return cbMesH;
+	}
+
+	private JComboBox<String> getCbAnoH() {
+		if (cbAnoH == null) {
+			cbAnoH = new JComboBox<String>();
+			cbAnoH.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			cbAnoH.setBounds(535, 47, 84, 25);
+		}
+		return cbAnoH;
+	}
+
+	private JLabel getLblHasta() {
+		if (lblHasta == null) {
+			lblHasta = new JLabel("Hasta:");
+			lblHasta.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblHasta.setBounds(329, 22, 59, 21);
+		}
+		return lblHasta;
+	}
+	
+	/**
+	 * Borra los elementos de la tabla
+	 */
+	private void borrarModelo() {
+		while (modeloTablaReservas.getRowCount() > 0) {
+			modeloTablaReservas.removeRow(0);
+		}
+	}
+
+	private JButton getBtnMostrarReserva() {
+		if (btnMostrarReserva == null) {
+			btnMostrarReserva = new JButton("Mostrar Reserva");
+			btnMostrarReserva.addActionListener(new ActionListener() {
+				@SuppressWarnings("deprecation")
+				public void actionPerformed(ActionEvent e) {
+					borrarModelo();
+					// llamar a un metodo de la base de datos que me coja las
+					// reservas del usuario entre las fechas seleccionadas
+					// comprobar que no este cancelada.
+					if (rdbtnPendientes.isSelected()) {
+						bd.cargarReservasPendientesUsuario(usuario, new Timestamp(año-1900, mes, dia, 0, 0, 0, 0),
+								new Timestamp(Integer.parseInt(cbAno.getItemAt(cbAno.getSelectedIndex())),
+										mes + cbMes.getSelectedIndex(),
+										Integer.parseInt(cbDia.getItemAt(cbDia.getSelectedIndex())), 0, 0, 0, 0));
+					} else if (rdbtnRealizadas.isSelected()) {
+						bd.cargarReservasRealizadasUsuario(usuario,
+								new Timestamp(Integer.parseInt(cbAno.getItemAt(cbAno.getSelectedIndex())),
+										mes + cbMes.getSelectedIndex(),
+										Integer.parseInt(cbDia.getItemAt(cbDia.getSelectedIndex())), 0, 0, 0, 0),
+								new Timestamp(Integer.parseInt(cbAno.getItemAt(cbAno.getSelectedIndex())),
+										mes + cbMes.getSelectedIndex(),
+										Integer.parseInt(cbDia.getItemAt(cbDia.getSelectedIndex())), 0, 0, 0, 0));
+					}else{
+						//Cargar las reservas canceladas.
+					}
+				}
+			});
+			btnMostrarReserva.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			btnMostrarReserva.setBounds(130, 206, 147, 23);
+		}
+		return btnMostrarReserva;
+	}
+
+	private JButton getBtnAnularReserva() {
+		if (btnAnularReserva == null) {
+			btnAnularReserva = new JButton("Anular Reserva");
+			btnAnularReserva.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String horaInicio = (String) tbReservas.getValueAt(tbReservas.getSelectedRow(), 1);
+					String horaFin = (String) tbReservas.getValueAt(tbReservas.getSelectedRow(), 2);
+					bd.cancelarReservaUsuario(usuario, horaInicio, horaFin);
+				}
+			});
+			btnAnularReserva.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			btnAnularReserva.setBounds(372, 206, 147, 23);
+		}
+		return btnAnularReserva;
+	}
+
+	private JButton getBtnAtras() {
+		if (btnAtras == null) {
+			btnAtras = new JButton("Atras");
+			btnAtras.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			btnAtras.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			btnAtras.setBounds(563, 530, 89, 23);
+		}
+		return btnAtras;
+	}
+
+	private JScrollPane getScrollPane_1() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(10, 240, 642, 279);
+			scrollPane.setViewportView(getTbReservas());
+		}
+		return scrollPane;
+	}
+
+	private JTable getTbReservas() {
+		if (tbReservas == null) {
+			modeloTablaReservas = new DefaultTableModel(new Object[] { "Sala", "Hora Inicio", "Hora Fin" }, 0);
+			tbReservas = new JTable(modeloTablaReservas);
+			tbReservas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			tbReservas.getTableHeader().setReorderingAllowed(false);
+		}
+		return tbReservas;
 	}
 }
--- src/logica/BaseDatos.java
+++ src/logica/BaseDatos.java
@@ -34,7 +34,7 @@ public class BaseDatos {
 		}
 
 	}
-	
+
 	@SuppressWarnings("serial")
 	public class ExcepcionReservaNoEncontrada extends Exception {
 		public ExcepcionReservaNoEncontrada(String msg) {
@@ -42,7 +42,7 @@ public class BaseDatos {
 		}
 
 	}
-	
+
 	@SuppressWarnings("serial")
 	public class ExcepcionPagoNoEncontrado extends Exception {
 		public ExcepcionPagoNoEncontrado(String msg) {
@@ -51,7 +51,7 @@ public class BaseDatos {
 
 	}
 
-	public Connection conectar() {
+	private Connection conectar() {
 		try {
 			conexion = DriverManager.getConnection(URL, USER, PASS);
 		} catch (SQLException e) {
@@ -70,9 +70,10 @@ public class BaseDatos {
 		else
 			throw new ExceptionUsuarioContraseña("EL usuario y la contraseña no coinciden.");
 	}
-	
+
 	/**
 	 * Comprobar administrador y contraseña.
+	 * 
 	 * @param user
 	 * @param password
 	 * @return
@@ -90,7 +91,7 @@ public class BaseDatos {
 		else
 			throw new ExceptionUsuarioContraseña("EL usuario y la contraseña no coinciden.");
 	}
-	
+
 	public Usuario cargarAdministrador(String identificador) {
 		try {
 			Usuario user = null;
@@ -110,7 +111,8 @@ public class BaseDatos {
 				double cuota = rs.getDouble("CUOTA");
 				boolean baja = rs.getBoolean("BAJA");
 				Timestamp fecha_baja = rs.getTimestamp("FECHA_BAJA");
-				user = new Usuario(user_id, dni, nombre, apellidos, direccion, email, ciudad, password,cuota,baja,fecha_baja);
+				user = new Usuario(user_id, dni, nombre, apellidos, direccion, email, ciudad, password, cuota, baja,
+						fecha_baja);
 			}
 			rs.close();
 			ps.close();
@@ -141,7 +143,8 @@ public class BaseDatos {
 				double cuota = rs.getDouble("CUOTA");
 				boolean baja = rs.getBoolean("BAJA");
 				Timestamp fecha_baja = rs.getTimestamp("FECHA_BAJA");
-				user = new Usuario(user_id, dni, nombre, apellidos, direccion, email, ciudad, password,cuota,baja,fecha_baja);
+				user = new Usuario(user_id, dni, nombre, apellidos, direccion, email, ciudad, password, cuota, baja,
+						fecha_baja);
 			}
 			rs.close();
 			ps.close();
@@ -218,8 +221,8 @@ public class BaseDatos {
 	public boolean comprobarReservaSala(String codigoSala, Timestamp horaInicio) {
 		try {
 			Connection con = conectar();
-			PreparedStatement ps = con.prepareStatement(
-					"SELECT R.ID_RESERVA FROM RESERVA r WHERE r.ID_SALA = ? AND r.HORA_INICIO = ?");
+			PreparedStatement ps = con
+					.prepareStatement("SELECT R.ID_RESERVA FROM RESERVA r WHERE r.ID_SALA = ? AND r.HORA_INICIO = ?");
 			ps.setString(1, codigoSala);
 			ps.setTimestamp(2, horaInicio);
 			ResultSet rs = ps.executeQuery();
@@ -272,7 +275,7 @@ public class BaseDatos {
 		}
 		return false;
 	}
-	
+
 	private int findID_Usuario(String DNI) throws ExcepcionUsuarioNoEncontrado {
 		Connection con = null;
 		PreparedStatement psID_USUARIO = null;
@@ -283,7 +286,7 @@ public class BaseDatos {
 			psID_USUARIO = con.prepareStatement("select id_usuario from usuario where DNI = ?");
 			psID_USUARIO.setString(1, DNI);
 			rsID_USUARIO = psID_USUARIO.executeQuery();
-			if(rsID_USUARIO.next()) {
+			if (rsID_USUARIO.next()) {
 				idUsuario = rsID_USUARIO.getInt(1);
 				rsID_USUARIO.close();
 				psID_USUARIO.close();
@@ -300,22 +303,22 @@ public class BaseDatos {
 		}
 		return idUsuario;
 	}
-	
+
 	private int findID_Reserva(int id_usuario, Timestamp fecha) throws ExcepcionReservaNoEncontrada {
 		Connection con = null;
 		PreparedStatement psID_RESERVA = null;
 		ResultSet rsID_RESERVA = null;
-		
+
 		int idReserva = 0;
-		
+
 		try {
 			con = conectar();
-			psID_RESERVA = con.prepareStatement("select id_reserva from reserva "
-					+ "where id_usuario = ? and hora_inicio = ?");
+			psID_RESERVA = con
+					.prepareStatement("select id_reserva from reserva " + "where id_usuario = ? and hora_inicio = ?");
 			psID_RESERVA.setInt(1, id_usuario);
 			psID_RESERVA.setTimestamp(2, fecha);
 			rsID_RESERVA = psID_RESERVA.executeQuery();
-			if(rsID_RESERVA.next()) {
+			if (rsID_RESERVA.next()) {
 				idReserva = rsID_RESERVA.getInt(1);
 				rsID_RESERVA.close();
 				psID_RESERVA.close();
@@ -332,20 +335,20 @@ public class BaseDatos {
 		}
 		return idReserva;
 	}
-	
-	private int findID_Pago( int idReserva) throws ExcepcionPagoNoEncontrado {
+
+	private int findID_Pago(int idReserva) throws ExcepcionPagoNoEncontrado {
 		Connection con = null;
 		PreparedStatement psID_PAGO = null;
 		ResultSet rsID_PAGO = null;
-		
+
 		int idPago = 0;
-		
+
 		try {
 			con = conectar();
 			psID_PAGO = con.prepareStatement("select id_pago from pago where id_reserva = ?");
 			psID_PAGO.setInt(1, idReserva);
 			rsID_PAGO = psID_PAGO.executeQuery();
-			if(rsID_PAGO.next()) {
+			if (rsID_PAGO.next()) {
 				idPago = rsID_PAGO.getInt(1);
 				rsID_PAGO.close();
 				psID_PAGO.close();
@@ -361,42 +364,43 @@ public class BaseDatos {
 		}
 		return idPago;
 	}
-	
-	public void pasarPagoAPagado( String DNI, Timestamp fecha ) throws ExcepcionUsuarioNoEncontrado, ExcepcionReservaNoEncontrada, ExcepcionPagoNoEncontrado, IOException {
-		Connection con = null;		
+
+	public void pasarPagoAPagado(String DNI, Timestamp fecha)
+			throws ExcepcionUsuarioNoEncontrado, ExcepcionReservaNoEncontrada, ExcepcionPagoNoEncontrado, IOException {
+		Connection con = null;
 		PreparedStatement psUPDATE_PAGO = null;
-		
+
 		int id_usuario = 0;
 		int id_reserva = 0;
 		int id_pago = 0;
-		
+
 		try {
 			con = conectar();
 			id_usuario = findID_Usuario(DNI);
 			id_reserva = findID_Reserva(id_usuario, fecha);
-			
+
 			psUPDATE_PAGO = con.prepareStatement("update pago set status = ? where id_reserva = ?");
 			psUPDATE_PAGO.setString(1, "pagado");
 			psUPDATE_PAGO.setInt(2, id_reserva);
 			int num = psUPDATE_PAGO.executeUpdate();
-			if( num == 0) {
+			if (num == 0) {
 				throw new ExcepcionPagoNoEncontrado("El pago no existe");
 			}
 			id_pago = findID_Pago(id_reserva);
 			generarRecibo(id_pago, DNI, fecha);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private double sacarImportePago(int idPago) {
 		Connection con = null;
 		PreparedStatement psIMPORTE_PAGO = null;
 		ResultSet rsIMPORTE_PAGO = null;
-		
+
 		double importe = 0.0;
-		
+
 		try {
 			con = conectar();
 			psIMPORTE_PAGO = con.prepareStatement("select precio from pago where id_pago = ?");
@@ -404,35 +408,153 @@ public class BaseDatos {
 			rsIMPORTE_PAGO = psIMPORTE_PAGO.executeQuery();
 			rsIMPORTE_PAGO.next();
 			importe = rsIMPORTE_PAGO.getDouble(1);
-			
+
 			rsIMPORTE_PAGO.close();
 			psIMPORTE_PAGO.close();
 			con.close();
-		} catch (SQLException e ) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return importe;
 	}
-	
-	private void generarRecibo( int id_pago, String DNI, Timestamp fecha) throws IOException {
+
+	private void generarRecibo(int id_pago, String DNI, Timestamp fecha) throws IOException {
 		int idPago = id_pago;
 		double importe = sacarImportePago(idPago);
-		
+
 		String ruta = id_pago + ".txt";
-        File archivo = new File(ruta);
-        BufferedWriter bw;
-        if(archivo.exists()) {
-            bw = new BufferedWriter(new FileWriter(archivo));
-            bw.write("Número de recibo: " + idPago);
-            bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
-            bw.write("Importe a pagar: " + importe + ".");
-        } else {
-            bw = new BufferedWriter(new FileWriter(archivo));
-            bw.write("Número de recibo: " + idPago);
-            bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
-            bw.write("Importe a pagar: " + importe + ".");
-        }
-        bw.close();
+		File archivo = new File(ruta);
+		BufferedWriter bw;
+		if (archivo.exists()) {
+			bw = new BufferedWriter(new FileWriter(archivo));
+			bw.write("Número de recibo: " + idPago);
+			bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
+			bw.write("Importe a pagar: " + importe + ".");
+		} else {
+			bw = new BufferedWriter(new FileWriter(archivo));
+			bw.write("Número de recibo: " + idPago);
+			bw.write("El socio con DNI: " + DNI + " ha pagado la reserva con fecha " + fecha + ".");
+			bw.write("Importe a pagar: " + importe + ".");
+		}
+		bw.close();
 	}
-	
+
+	/**
+	 * Metodo que recupera de la base de datos las reservas de un determinado
+	 * usuario que todavia no han pasado.
+	 * 
+	 * @param user
+	 * @param fechaActual
+	 * @param fechaHasta
+	 * @return
+	 */
+	public List<String> cargarReservasPendientesUsuario(Usuario user, Timestamp fechaActual, Timestamp fechaHasta) {
+		List<String> reservasPendientes = new ArrayList<String>();
+		try {
+			Connection con = conectar();
+			// Falta añadir la comprobacion de que no este cancelada.
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT S.DESCRIPCION,R.HORA_INICIO,R.HORA_FIN FROM RESERVA R,SALA S WHERE S.ID_SALA = R.ID_SALA AND R.ID_USUARIO = ? AND R.HORA_INICIO BETWEEN ? AND ?;");
+			ps.setInt(1, user.getId_usuario());
+			ps.setTimestamp(2, fechaActual);
+			ps.setTimestamp(3, fechaHasta);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				reservasPendientes.add(rs.getString(1) + ";" + rs.getString(2) + ";" + rs.getString(3));
+			}
+			rs.close();
+			ps.close();
+			con.close();
+			return reservasPendientes;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Metodo que recupera de la base de datos las reservas que ha realizado un
+	 * usuario con independencia de que hayan o no pasado o esten o no
+	 * canceladas.
+	 * 
+	 * @param user
+	 * @param fechaDesde
+	 * @param fechaHasta
+	 * @return
+	 */
+	public List<String> cargarReservasRealizadasUsuario(Usuario user, Timestamp fechaDesde, Timestamp fechaHasta) {
+		List<String> reservasPendientes = new ArrayList<String>();
+		try {
+			Connection con = conectar();
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT S.DESCRIPCION,R.HORA_INICIO,R.HORA_FIN FROM RESERVA R,SALA S WHERE S.ID_SALA = R.ID_SALA AND R.ID_USUARIO = ? AND R.HORA_INICIO BETWEEN ? AND ?;");
+			ps.setInt(1, user.getId_usuario());
+			ps.setTimestamp(2, fechaDesde);
+			ps.setTimestamp(3, fechaHasta);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				reservasPendientes.add(rs.getString(1) + ";" + rs.getString(2) + ";" + rs.getString(3));
+			}
+			rs.close();
+			ps.close();
+			con.close();
+			return reservasPendientes;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Metodo que recupera de la base de datos las reservas de un usuario que
+	 * han sido canceladas.
+	 * 
+	 * @param user
+	 * @param fechaDesde
+	 * @param fechaHasta
+	 * @return
+	 */
+	public List<String> cargarReservasCanceladasUsuario(Usuario user, Timestamp fechaDesde, Timestamp fechaHasta) {
+		List<String> reservasPendientes = new ArrayList<String>();
+		try {
+			Connection con = conectar();
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT S.DESCRIPCION,R.HORA_INICIO,R.HORA_FIN FROM RESERVA R,SALA S WHERE S.ID_SALA = R.ID_SALA AND R.ID_USUARIO = ? AND R.ESTADO = 'CANCELADA' AND R.HORA_INICIO BETWEEN ? AND ?;");
+			ps.setInt(1, user.getId_usuario());
+			ps.setTimestamp(2, fechaDesde);
+			ps.setTimestamp(3, fechaHasta);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				reservasPendientes.add(rs.getString(1) + ";" + rs.getString(2) + ";" + rs.getString(3));
+			}
+			rs.close();
+			ps.close();
+			con.close();
+			return reservasPendientes;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Metodo que cambia el estado de una reserva a cancelado.
+	 * 
+	 * @param user
+	 * @param fechaDesde
+	 * @param fechaHasta
+	 */
+	public void cancelarReservaUsuario(Usuario user, String fechaDesde, String fechaHasta) {
+		try {
+			Connection con = conectar();
+			PreparedStatement ps = con.prepareStatement(
+					"UPDATE RESERVAS R SET ESTADO ='CANCELADA' WHERE R.ID_USUARIO=? AND R.HORA_INICIO = ? AND R.HORA_FIN = ?");
+			ps.setInt(1, user.getId_usuario());
+			ps.setString(2, fechaDesde);
+			ps.setString(3, fechaHasta);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
